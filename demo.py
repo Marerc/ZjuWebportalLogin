f69ce3c31f6399a627a6236df7c6330f257e4b5b
miniat <lyfee@qq.com>
2021-01-23 13:42:08 +0800

    增加了图片说明

--- demo.py
+++ demo.py
@@ -1,10 +1,12 @@
 from BitSrunLogin.LoginManager import LoginManager
 
+loginDomainOrIp = "10.92.110.107"
+
 # zs校区可以不改地址，只需要填你自己的帐号密码就行
 lm = LoginManager(
-	url_login_page = "http://10.92.110.107/srun_portal_pc?ac_id=1&theme=zju",
-	url_get_challenge_api = "http://10.92.110.107/cgi-bin/get_challenge",
-	url_login_api = "http://10.92.110.107/cgi-bin/srun_portal")
+	url_login_page = "http://%s/srun_portal_pc?ac_id=1&theme=zju"%loginDomainOrIp,
+	url_get_challenge_api = "http://%s/cgi-bin/get_challenge"%loginDomainOrIp,
+	url_login_api = "http://%s/cgi-bin/srun_portal"%loginDomainOrIp)
 
 lm.login(
     username = "2183333333",
